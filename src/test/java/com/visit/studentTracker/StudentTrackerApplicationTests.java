6d6d373f2212b1c5e290d409883963de249126ef
hyunmook-lim <hmlim0320@naver.com>
2025-07-22 07:10:09 +0900

    프로젝트 최초 커밋

---
+++ src/test/java/com/visit/studentTracker/StudentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visit.studentTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
